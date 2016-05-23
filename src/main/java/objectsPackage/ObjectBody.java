926d6d1828405668d981ad7bb66d7672b91b8998
unknown <chayalaglazer@gmail.com>
2016-05-23 19:35:12 -0400

    object body

---
+++ src/main/java/objectsPackage/ObjectBody.java
@@ -0,0 +1,14 @@
+package objectsPackage;
+
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
+
+public class ObjectBody extends Body {
+
+	public ObjectBody(BodyDef bd, World world) {
+		super(bd, world);
+		// TODO Auto-generated constructor stub
+	}
+
+}
